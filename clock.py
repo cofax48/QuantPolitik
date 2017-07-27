3998e330b716b21f0c3559238207ae7e4c54761f
cofax48 <cofax48@uchicago.edu>
2017-07-27 14:17:10 -0400

    clock fix?

--- clock.py
+++ clock.py
@@ -5,19 +5,23 @@ from rq import Queue
 from worker import conn
 import os
 
-from trigger_for_clock import trigger_execution as trigger
-
 import logging
 import sys
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-sched = BlockingScheduler()
 
 q = Queue(connection=conn)
 
 def gather_trigger():
+    from trigger_for_clock import trigger_execution as trigger
     print('Triggering')
     q.enqueue(trigger)
 
-sched.add_job(gather_trigger, 'cron', day_of_week='*', hour=7, minute=21, timezone='US/Eastern')
-sched.start()
+if __name__ == '__main__':
+    sched = BlockingScheduler()
+    sched.add_job(gather_trigger, 'cron', day_of_week='*', hour=7, minute=21, timezone='US/Eastern')
+
+    try:
+        sched.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
--- trigger_for_clock.py
+++ trigger_for_clock.py
@@ -1,9 +1,15 @@
 def trigger_execution():
-    from Trump_New_Schedule_Getter import main as PresMain
-    print('PresMain Run')
-    from sec_state_schedule_quantifier import main as SECMain
-    print('SecState Run')
-    from QP_SCORE_MAKER import main as QP_Main
-    print('QP Score Maker Run')
+    def Pres():
+        from Trump_New_Schedule_Getter import main as PresMain
+        print('PresMain Run')
+        return 'PresMain Has Run'
+    def SecState():
+        from sec_state_schedule_quantifier import main as SECMain
+        print('SecState Run')
+        return 'SecState Has Run'
+    def QPMAIN():
+        from QP_SCORE_MAKER import main as QP_Main
+        print('QP Score Maker Run')
+        return 'QPMAIN has run'
 
-trigger_execution()
+    return 'They have all run'
