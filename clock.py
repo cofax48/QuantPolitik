f0d340010fc64add0cc681ba2ffc90c9d80e0114
cofax48 <cofax48@uchicago.edu>
2017-07-28 15:04:59 -0400

    mo mo mo fix

--- clock.py
+++ clock.py
@@ -13,14 +13,19 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 q = Queue(connection=conn)
 
 def gather_trigger():
-    from trigger_for_clock import trigger_execution as trigger
-    trigger()
-    print('Triggering')
-    q.enqueue(trigger)
+    from Trump_New_Schedule_Getter import main as PresMain
+    q.enqueue(PresMain)
+    print('PresMain Run')
+    from sec_state_schedule_quantifier import main as SECMain
+    q.enqueue(SECMain)
+    print('SecState Run')
+    from QP_SCORE_MAKER import main as QP_Main
+    q.enqueue(QP_Main)
+    print('QP Score Maker Run')
 
 if __name__ == '__main__':
     sched = BlockingScheduler()
-    sched.add_job(gather_trigger, 'cron', day_of_week='*', hour=14, minute=40, timezone='US/Eastern')
+    sched.add_job(gather_trigger, 'cron', day_of_week='*', hour=15, minute=10, timezone='US/Eastern')
 
     try:
         sched.start()
