b4369e30aa07b890ddf1381839e09b10734f93d0
cofax48 <cofax48@uchicago.edu>
2017-07-28 13:28:09 -0400

    small fix

--- hello/static/assets/js/7MinuteWorkOut/app.js
+++ hello/static/assets/js/7MinuteWorkOut/app.js
@@ -1,4 +1,20 @@
-angular.module('app', ['7minWorkout']);
+angular.module('app', ['ngRoute', '7minWorkout']).
+config(function ($routeProvider) {
+  $routeProvider.when('/start', {
+    templateUrl: 'partials/start.html'
+  });
+  //accessible under http://<hostname>/index.html#/start
+  $routeProvider.when('/workout', {
+    templateUrl: 'partials/workout.html',
+    controller: 'WorkoutController'
+  });
+  $routeProvider.when('/finish', {
+    templateUrl: 'partials/finish.html'
+  });
+  $routeProvider.otherwise({
+    redirectTo: '/start'
+  });
+});
 
 angular.module('7minWorkout', []);
 //angular.module('7minWorkout', []); // creates a new module
--- trigger_for_clock.py
+++ trigger_for_clock.py
@@ -1,8 +1,10 @@
 def trigger_execution():
     def Pres():
         from Trump_New_Schedule_Getter import main as PresMain
+        PresMain()
         print('PresMain Run')
         return 'PresMain Has Run'
+    """
     def SecState():
         from sec_state_schedule_quantifier import main as SECMain
         print('SecState Run')
@@ -11,5 +13,5 @@ def trigger_execution():
         from QP_SCORE_MAKER import main as QP_Main
         print('QP Score Maker Run')
         return 'QPMAIN has run'
-
+    """
     return 'They have all run'
